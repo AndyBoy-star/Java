d8ff88dbbfe4113171dabca648072fbc5c1251b8
Andrii Boiko <andyboy7777777@gmail.com>
2024-09-26 20:29:44 +0200

    Homework_1

---
+++ Homework_1/src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        char ch = 'G';
+        System.out.println(ch);
+        int num1 = 89;
+        System.out.println(num1);
+        byte num2 = 4;
+        System.out.println(num2);
+        short num3 = 56;
+        System.out.println(num3);
+        float num4 = 4.7333436F;
+        System.out.println(num4);
+        double num5 = 4.355453532;
+        System.out.println(num5);
+        long num6 = 12121;
+        System.out.println(num6);
+        Character ch1 = 'G';
+        System.out.println(ch1);
+        Integer num7 = 89;
+        System.out.println(num7);
+        Byte num8 = 4;
+        System.out.println(num8);
+        Short num9 = 56;
+        System.out.println(num9);
+        Float num10 = 4.7333436F;
+        System.out.println(num10);
+        Double num11 = 4.355453532;
+        System.out.println(num11);
+        Long num12 = 12121L;
+        System.out.println(num12);
+
+
+        System.out.println("Число 345 " + "-->" + " 3, 4, 5");
+    }
+}
+
+
+
+
+//1. В методе main инициализировать все примитивные типы и не примитивные типы.
+//2. Вывести их результат в консоль:
+//char: G
+//int: 89
+//byte: 4
+//short: 56
+//float: 4.7333436
+//double: 4.355453532
+//long: 12121
+//Character: G
+//Integer: 89
+//Byte: 4
+//Short: 56
+//Float: 4.7333436
+//Double: 4.355453532
+//Long: 12121
