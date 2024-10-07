cd7c09bb0eee83b128fd40134d8192904db92f18
Andrii Boiko <andyboy7777777@gmail.com>
2024-10-07 22:26:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,3 +13,21 @@ public class Main {
 
     }
 }
+
+
+import java.util.Scanner;
+
+public class Converter {
+    static double convertToUsd(double eur){
+
+        return eur * 1.15;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter amount in euro");
+        double eur = sc.nextDouble();
+        double usd = convertToUsd(eur);
+        System.out.println("Dollar  amount: " + usd);
+    }
+}
